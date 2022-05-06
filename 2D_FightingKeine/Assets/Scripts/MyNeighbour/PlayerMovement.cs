5289ee94c8693a44206c3ba93076b6db2fa83eab
Hai Lam Dang <hai.dang@ue-germany.de>
2022-05-06 18:34:48 +0200

    [CHANGE] Working Yarn with start dialog at will

--- 2D_FightingKeine/Assets/Scripts/MyNeighbour/PlayerMovement.cs
+++ 2D_FightingKeine/Assets/Scripts/MyNeighbour/PlayerMovement.cs
@@ -15,6 +15,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private SpriteRenderer playerSpriteRenderer;
 
+    //[SerializeField]
+    private bool canPlayerMove = true;
+
     [Header("Mouse and Cursor's Value")]
     [SerializeField]
     private MouseClickPosition mouseManager;
@@ -34,38 +37,53 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //Quick DOTween movement -> very rigid but works
-        player.transform.DOMoveX(mouseManager.MousePositionValue.x , tweenDuration , isTweenSnapOn);
-
-        lastMouseClickPos = mouseManager.MousePositionValue;
-        playerCurrentPos = player.transform.position;
-
-        //Now check: if Mouse click Right -> Mouse's x-Value bigger than playerCurrentPos
-        if(playerCurrentPos.x < lastMouseClickPos.x)
+        if(canPlayerMove == true)
         {
-            playerSpriteRenderer.flipX = false;
-            //Debug.Log("Player moving Right");
+            //Quick DOTween movement -> very rigid but works
+            player.transform.DOMoveX(mouseManager.MousePositionValue.x , tweenDuration , isTweenSnapOn);
+
+            lastMouseClickPos = mouseManager.MousePositionValue;
+            playerCurrentPos = player.transform.position;
+
+            //Now check: if Mouse click Right -> Mouse's x-Value bigger than playerCurrentPos
+            if (playerCurrentPos.x < lastMouseClickPos.x)
+            {
+                playerSpriteRenderer.flipX = false;
+                //Debug.Log("Player moving Right");
+            }
+
+            //Mouse click Left -> Mouse's x-Value smaller than playerCurrentPos
+            else
+            {
+                playerSpriteRenderer.flipX = true;
+                //Debug.Log("Player moving Left");
+            }
+
+            //In Space movement!!! (Like a real Ninja)
+            /*if (mouseManager.IsPlayerMoving && (Vector2)transform.position != lastClickPos)
+            {
+                //Check if we are in the lastClickPos. If FALSE, move to it.
+                float step = playerSpeed * Time.deltaTime;
+                playerRigidBody.transform.position = Vector2.MoveTowards(player.transform.position, lastClickPos, step);
+            }
+
+            else
+            {
+                //If reach lastClickPos -> Can't move until mouseClick again
+                mouseManager.IsPlayerMoving = false;
+            }*/
         }
 
-        //Mouse click Left -> Mouse's x-Value smaller than playerCurrentPos
-        else
-        {
-            playerSpriteRenderer.flipX = true;
-            //Debug.Log("Player moving Left");
-        }
+    }
 
-        //In Space movement!!! (Like a real Ninja)
-        /*if (mouseManager.IsPlayerMoving && (Vector2)transform.position != lastClickPos)
-        {
-            //Check if we are in the lastClickPos. If FALSE, move to it.
-            float step = playerSpeed * Time.deltaTime;
-            playerRigidBody.transform.position = Vector2.MoveTowards(player.transform.position, lastClickPos, step);
-        }
+    //public method for DialogueRunner -> Can't move when dialog playing
+    public void ChangePlayerMoveTrue()
+    {
+        canPlayerMove = true;
+    }
 
-        else
-        {
-            //If reach lastClickPos -> Can't move until mouseClick again
-            mouseManager.IsPlayerMoving = false;
-        }*/
+    public void ChangePlayerMoveFalse()
+    {
+        canPlayerMove = false;
     }
 }
